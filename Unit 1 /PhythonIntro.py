88576191b245a394f071bc426b8bce35a82d75f5
Sebastian H <shernandez959@boyslatin.net>
2026-02-11 13:45:12 +0000

    completed intro work

---
+++ Unit 1 /PhythonIntro.py
@@ -0,0 +1,30 @@
+BankAccount = 11223.95
+groceryTotal = 453.79
+numberofAssignment = 8
+numberofPizzas = 10 
+
+# Data types = the most fundamental componet of programing 
+
+# Variable = a container for data (*data = data types)
+
+# Integer = a whole numbher 
+
+# Float = a decimal number 
+
+# String = anything wrapped around quotations is a string data type
+# Data "string" together by quotations
+
+"hello
+"About my grade"
+
+# Boolean = represent true or false values
+
+x=19
+y=20
+
+print("hey areyou going to the study hall later")
+print(x - y)
+
+print(True) 
+
+print(y + x) 
