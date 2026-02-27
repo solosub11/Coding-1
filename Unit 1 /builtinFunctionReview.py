2422cd4f1a9df9e4f4acd066942dcb964d24a70f
Sebastian H <shernandez959@boyslatin.net>
2026-02-27 13:47:52 +0000

    Im done with the reivew.

---
+++ Unit 1 /builtinFunctionReview.py
@@ -0,0 +1,23 @@
+#1. age = "16"
+print(type(16))
+print(type(int(16)))
+
+#2.  score = 85 
+print(type(85))
+print(type(str(85)))
+
+#3. price = 19.99 
+print(type(19.99))
+print(type(float(19.99)))
+
+#4.
+num1= input(type(4))
+num2 = input(type(str(5)))
+
+
+
+#5. 
+print('it will cost' + str(120.00 ) + 'dollars to get everyone lunch')
+
+#6. 
+print(100+4)
