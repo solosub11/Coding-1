47508436de36c39f393f56e688b4157725e0f02a
Sebastian H <shernandez959@boyslatin.net>
2026-03-06 13:48:26 +0000

    complete

---
+++ Unit 2/FunctionExercise.py
@@ -0,0 +1,17 @@
+#1 "def" 
+
+#2 "The difference between a paramter and an agurment is that a parameters is a place holder for data that is not real,
+# while agurments are real data used in a function call that also needs facts."
+
+#3 
+def calculate(num1, num2):
+    print(num1 * num2)
+
+calculate(10,20)
+
+#4 
+def name(): 
+    print('insert a name and a number')
+    name = input('Sebastian', 10)
+    
+name()
