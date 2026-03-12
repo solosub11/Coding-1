aa57b09cba1a7bfb5e31695ab40c27294bc81bb9
Sebastian H <shernandez959@boyslatin.net>
2026-03-12 12:45:34 +0000

    Completed the assignments

---
+++ Unit 2/errorHandling.py
@@ -0,0 +1,46 @@
+# Syntax Error 
+"Syntax = how we write code. the symbols, characters, etc"
+"syntax error = the computer detected an issue with how we wrote"
+"so code"
+
+# Type Error 
+"type = means data type"
+"type error just means data types error"
+"type error means that there is an issue with our data types"
+
+print("I am" + 4 + 'years old')
+
+print("it is" + str(True) + "that im in school")
+
+print(23 + "4")
+
+# Name Error 
+"Name error usually is reserved for variables. These occur when"
+"you do not define a variable or function."
+
+a= 1 
+b = 2 
+c= a +b 
+
+# Indentation Error 
+'An issue that occurs when you do not ident AFTER'
+'making a function'
+
+def runCode():
+    print("do something")
+    print("do something else ...")
+
+runCode()
+
+# Steps to fixing errors
+
+#1. Read the error. Do not get overwelmed. There will tell you 
+#what you need to fix 
+
+# 2. If you do not understand the error message - 
+# Look it up (w3schools, google, etc.)
+
+# 3. If you are still having issues and it impacts your code, 
+# comment the code out and "get a second pair of eyes"
+
+# 4. Use your generativer AI tools to assist you NOT to THINK for you.  
---
+++ Unit 2/functionReview.py
@@ -0,0 +1,23 @@
+# 1.
+"parameters are placeholders for data in the function definition."
+"agrumetns are real data that runs in the function call."
+
+
+#2. 
+def bookChallenge(numofBooks, titleofFav):
+    print("I need" + str(numofBooks) + "books this year")
+    print("my favorite book was" + titleofFav) 
+
+bookChallenge(390, "Jackie Robinson")
+
+#3. 
+"Syntax error - means that we broke the grammar rules. Wrote something wrong. Wrote something that the computer doewsn't undersdtand."
+"name error - this means the computer cant find a variable or function name because it was not defined/ the computer was not told what it means."
+
+#4. 
+def calculate():
+    number = int(input("please type in a number")) 
+    sum = number * number /2 +5 
+    print(sum) 
+
+calculate()
