55d7bb1bf54579cf01309d0bba2fb19d058126d6
Sebastian H <shernandez959@boyslatin.net>
2026-04-16 12:46:17 +0000

    completed List notes

---
+++ Unit 3 /List.py
@@ -0,0 +1,18 @@
+# A list is a container for multiple 
+# pieces of data(data types)
+
+# Its sort of like a variable, but you can put more 
+# data in it. 
+
+name = "Ian"
+Score = 100 
+
+# list Syntax 
+# 1. give the list a name, similar to what we do with
+# variables
+# 2. Add 2 square brackets []
+# 3. Put data types inside the brackets, separated by coms
+coding1Class = ["bill,"'sarah', 'mark'] 
+# Lists can have ANY data type inside of the brackets 
+# including other lists
+
